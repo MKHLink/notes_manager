d47514ecb4b0fd62c684c1be5964f9d6f11df59a
Aleph Null <[likhonhasankomol@gmail.com]>
2022-10-17 14:29:59 -0400

    can post data in the db json file

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
 const {notes} = require('./db/db.json');
@@ -10,6 +16,20 @@ function displayNote(id,notesArr)
 {
     const display = notesArr.filter(note => note.id === id)[0];
     return display;
+}
+
+//function to add note
+function createNote(body, notesArr)
+{
+    const note = body;
+    notesArr.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes : notesArr}, null , 2)
+    );
+
+    return note;
 }
 
 app.get('/notes/:id',(req,res)=>{
@@ -27,6 +47,14 @@ app.get('/notes/:id',(req,res)=>{
 
 app.get('/notes',(req,res)=>{
     res.json(notes);
+});
+
+app.post('/notes',(req,res)=>{
+    req.body.id = notes.length.toString();
+
+    const note = createNote(req.body,notes);
+
+    res.json(note);
 });
 
 app.listen(PORT,()=>{
